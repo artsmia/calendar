4768c94cda7018a02aa3ccc688ef2818f4bc2c5e
Kjell <kjell@leanside.com>
2014-03-05 10:32:39 -0600

    color the boxes and show event info

--- bundle.js
+++ bundle.js
@@ -16,9 +16,35 @@ var y = d3.scale.linear()
   .domain([0, cal.instances.length])
   .range([0, h])
 
+var typeColor = {
+  "classes": "magenta",
+  "family-events": "orange",
+  "film-and-performance": "purple",
+  "lectures-and-seminars": "blue",
+  "special-events": "red",
+  "tours": "green",
+  "exhibitions": "chartreuse"
+}
+
+var eventHtml = function(d) {
+  var dateFrom = new Date(d.dateFrom*1000)
+    , dateTo = new Date(d.dateTo*1000)
+    , dates = (dateFrom === dateTo ? [dateFrom] : [dateFrom, dateTo])
+    , dateString = dates.map(function(date) {
+      return date.toLocaleString().split(" ")[0]
+    }).join(" — ")
+
+  return "<h3><a href='"+d.permalink+"'>"+d.title+"</a></h3>" +
+    "<p>"+d.typeName+". "+dateString+"</p>"
+}
+
 var svg = d3.select("body").append("svg")
   .attr("width", w)
   .attr("class", "timeline")
+
+var div = d3.select("body").append("div")
+  .attr("class", "event")
+  .style("opacity", 0);
 
 var instances = svg.selectAll('.instance')
     .data(cal.instances)
@@ -28,6 +54,11 @@ var instances = svg.selectAll('.instance')
     .attr("y", function(d, i) { return y(i) })
     .attr("height", 5)
     .attr("width", function(d) { return Math.max(x(d.dateTo) - x(d.dateFrom), 5) })
+    .attr('fill', function(d, i) { return typeColor[d.typeCategory] })
+  .on('mouseover', function(d, i) {
+    div.html(eventHtml(d))
+    div.transition().duration(200).style("opacity", "0.9")
+  })
 
 window.api = {
   cal: cal,
