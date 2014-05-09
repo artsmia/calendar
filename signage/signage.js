90a0f4df70f97c6cde3e8db173cd75fe30fcea23
Kjell <kjell@leanside.com>
2014-05-09 11:47:13 -0500

    css for event opacity; anchor to bottom when few

--- signage/signage.js
+++ signage/signage.js
@@ -7,11 +7,6 @@ var list = d3.select("body").append("ul"),
     pastEvents = today.earlier(events)
     eventHtml = require('../eventHtml')
 
-var opacity = d3.scale.linear()
-  .range([0, 1])
-  .domain([events[events.length-1].dateFrom, new Date/1000])
-  .clamp(true)
-
 list.attr("id", "pulse")
 
 function update(events) {
@@ -23,10 +18,6 @@ function update(events) {
   events
       .html(function(d) {
         return eventHtml(d, true)
-      })
-      .style("opacity", function(d) {
-        // TODO: should this happen in css based on classes {past, now, future}?
-        return Date.create(d.timeFrom).advance(15*60*1000) > new Date ? 1 : 0.5
       })
       .classed("now", function(d) { return d.now })
       .classed("future", function(d) { return d.future })
@@ -50,6 +41,7 @@ if(window.location.hash != '#all') {
     pnf.past.forEach(function(e) { e.past = true })
 
     events = pnf.past.last(3).concat(pnf.now).concat(pnf.future)
+    list.classed('few', function() { return events.length < 5 })
     update(events)
     if(window.location.hash != 'freeze') return clearInterval(updateLoop)
   }, 1000)
