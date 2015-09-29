ac63d0893102f00d7a2e48fea537c769a0660e03
Kjell <kjell@leanside.com>
2015-09-29 10:56:07 -0500

    oct ober

--- graph/bundle.js
+++ graph/bundle.js
@@ -17,9 +17,10 @@ module.exports = function(d, excludeDate) {
 
 },{}],2:[function(require,module,exports){
 var nets = require('nets')
+var url = 'https://artsmia.github.io/calendar/calendar.json'
 
 module.exports = function(callback) {
-  nets({url: 'https://artsmia.github.io/calendar/calendar.json', encoding: 'utf8'}, function(err, resp, body) {
+  nets({url: url, encoding: 'utf8'}, function(err, resp, body) {
     var cal = JSON.parse(body)
       , sortedStart = cal.slice(0).sort(function(a, b) { return a.dateFrom - b.dateFrom })
       , sortedEnd = cal.slice(0).sort(function(a, b) { return a.dateTo - b.dateTo })
